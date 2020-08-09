c977260df2afdf3c642ece1f96d3427449f1cd65
delta12kilo <dk9044552577@gmail.com>
2020-08-09 13:20:26 +0530

    New changes

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 from .models import Post
 # Register your models here.
 
-admin.site.register(Post)
+# admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title','slug','author','publish','status')
+    list_filter = ('status','created','publish','author')
+    search_filter = ('title','body')
+    prepopulatedFeild = {'slug':('title',)}
+    date_h = 'publish'
+    ordering = ('status','publish')
--- blog/models.py
+++ blog/models.py
@@ -1,22 +1,29 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 # Create your models here.
 
 
 class Post(models.Model):
-    author = models.ForeignKey('auth.User',on_delete=models.CASCADE)
-    title = models.CharField(max_length=50)
-    text = models.TextField()
-    created_data = models.DateTimeField(
-        default=timezone.now
-    )
-    published_data = models.DateTimeField(
-        blank=True, null=True
-    )
+    STATUS_CHOICES = (
+('draft', 'Draft'),
+('published', 'Published'),
+)
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250,
+unique_for_date='publish')
+    author = models.ForeignKey(User,
+on_delete=models.CASCADE,
+related_name='blog_posts')
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10,
+choices=STATUS_CHOICES,
+default='draft')
+class Meta:
+    ordering = ('-publish',)
 
-    def publish(self):
-        self.published_data = timezone.now()
-        self.save()
-    
-    def __str__(self):
-        return self.title
+def __str__(self):
+    return self.title
