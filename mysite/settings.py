adc1c15caead16bf38a4503089789937aa6c50f9
delta12kilo <dk9044552577@gmail.com>
2020-08-16 09:21:54 +0000

    check

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'smk6&2qzoy#w@9o@4_(dnv1!!ygu8%he&x)zi5cq3c3k=ftr@('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['frogman12.pythonanywhere.com']
 
 
 # Application definition
@@ -119,7 +119,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+# STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR,'static'),
+)
+STATIC_ROOT =   os.path.join(BASE_DIR,'staticfiles')
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
